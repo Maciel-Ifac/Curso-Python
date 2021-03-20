17a948e65d5db1ef437fa7048fdb1fd92091b031
MacielUEl <antonio.msilva@ifac.edu.br>
2021-03-20 16:25:51 -0500

    Update exe1.1.py

--- capitulo2/exe1.1.py
+++ capitulo2/exe1.1.py
@@ -9,17 +9,15 @@ import numpy as np
 def f(x):
     return x**2 + 0.25*x - 5
 
-
 # Plotando a função
 '''
 x = np.linspace(-10, 10)
-
 plt.grid()
 plt.plot(x, f(x))
 plt.xlim(-10, 10)
 plt.ylim(-5, 93)
 plt.show()
-
 '''
 # Imprimindo resultado
-print(f(2.3))print("O valor de x é: {0:.5f}".format(f(2.11456)))
+print(f(2.11))
+print("O valor de x é: {0:.5f}".format(f(2.11456)))
