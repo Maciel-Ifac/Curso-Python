bd35fd36b7df7ae89af6c794ea449afc9806e55a
MacielUEl <antonio.msilva@ifac.edu.br>
2021-03-27 10:22:43 -0500

    Update exe1.py

--- capitulo3/exe1.py
+++ capitulo3/exe1.py
@@ -1,3 +1,14 @@
 # Exercício 1
 
+# 1.1
+L = [1, 2]
+L3 = 3*L
+print(L3)
+
+# 1.2
+print(L3[0],L3[-1]) # L3[10] gera um erro, pois não existe nesta lista!
+
+# 1.3
+L4 = [k**2 for k in L3] # Eleva cada elemento da lista ao quadrado.
+print(L4)
 
