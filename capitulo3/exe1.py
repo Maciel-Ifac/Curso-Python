1bb8b0e476fc1c3fc7c2c10c9b2a627f88ebd4ca
MacielUEl <antonio.msilva@ifac.edu.br>
2021-03-27 10:41:34 -0500

    Update exe1.py

--- capitulo3/exe1.py
+++ capitulo3/exe1.py
@@ -12,3 +12,9 @@ print(L3[0],L3[-1]) # L3[10] gera um erro, pois não existe nesta lista!
 L4 = [k**2 for k in L3] # Eleva cada elemento da lista ao quadrado.
 print(L4)
 
+# 1.4. Podemos concatenar L3 e L4 de várias formas. Segue dois exemplos:
+# Somando
+#L5 = L3 + L4
+# Por meio da função extend()
+L5 =  L3.extend(L4)
+print(L5)
